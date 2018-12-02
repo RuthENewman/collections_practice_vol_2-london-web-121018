0179a79e897f6c748894bf33a7f3d646237b5903
ruthnewman <ruthnewman@gmail.com>
2018-12-02 21:41:47 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,72 @@
-# your code goes here
+def begins_with_r(array)
+  counter = 0 
+  array.all? do |item| 
+    item[0] == 'r'
+  end   
+end 
+
+def contain_a(array)
+  new_array = []
+  array.each do |item|
+    if item.include?("a")
+      new_array << item
+    end 
+  end 
+    new_array
+  end 
+  
+def first_wa(array)
+  array.find do |item| 
+    item[0] == 'w' && item[1] == 'a'
+  end 
+end 
+
+def remove_non_strings(array)
+  array.delete_if do |item| 
+    item.class != String
+  end 
+ array
+end 
+
+
+def count_elements(array)
+  new_array = []
+  counter = 0
+  array.each do |item|
+    counter += 1 
+    item_value = counter
+element = {count: item_value, name: item.name}
+element_counter = 0
+while array.length < element_counter do
+  new_array << element 
+end 
+end 
+end 
+
+def merge_data
+  
+end 
+
+def find_cool
+
+
+end   
+  
+def 
+  
+  
+end 
+
+
+
+
+
+  
+  
+  
+
+
+
+
+
+
